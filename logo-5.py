7edb9e25a3de8e5772ba2771f399ba4e03e5b53b
Jean Tissot <asphjt@gmail.com>
2022-01-21 10:06:36 +0100

    add output folder

--- logo-5.py
+++ logo-5.py
@@ -1,12 +1,14 @@
-import ply.lex as lex 
-import ply.yacc as yacc
 import itertools as it
-from sys import argv
+import os
 from copy import copy
-
+from sys import argv
 from turtle import Turtle
+from unittest.mock import patch
 
-#-------------------------------------------------------------------------------
+import ply.lex as lex
+import ply.yacc as yacc
+
+# -------------------------------------------------------------------------------
 # Classes représentant des instructions du langage logo. L'analyse va créer des
 # listes de ces instructions. À la fin, on engendre le code de chacune d'elle
 # en séquence pour créer l'image.
@@ -22,6 +24,7 @@ class Forward:
     def instantiate(self, params):
         return Forward(self.arg.instantiate(params))
 
+
 class Right:
     def __init__(self, d):
         self.arg = d
@@ -31,6 +34,7 @@ class Right:
 
     def instantiate(self, params):
         return Right(self.arg.instantiate(params))
+
 
 class PenColor:
     def __init__(self, d):
@@ -42,6 +46,7 @@ class PenColor:
     def instantiate(self, params):
         return self
 
+
 class SetPen:
     def __init__(self, b):
         self.arg = b
@@ -51,6 +56,7 @@ class SetPen:
 
     def instantiate(self, params):
         return self
+
 
 class Value:
     def __init__(self):
@@ -63,6 +69,7 @@ class Value:
     def instantiate(self, params):
         return self
 
+
 class Parameter(Value):
     def __init__(self, n):
         Value.__init__(self)
@@ -74,24 +81,28 @@ class Parameter(Value):
         except KeyError:
             print('Erreur : paramètre inconnu', self.name)
             exit(1)
-        
+
         r.coeff = self.coeff
         return r
 
-class Constant(Value): # Constant hérite de Value
+
+class Constant(Value):  # Constant hérite de Value
     def __init__(self, d):
-        Value.__init__(self) # constructeur de la class mère à appeler explicitement
+        # constructeur de la class mère à appeler explicitement
+        Value.__init__(self)
         self.val = d
 
     def value(self):
         return self.coeff * self.val
 
-class XCor(Value): 
+
+class XCor(Value):
     def __init__(self):
         Value.__init__(self)
 
     def value(self):
         return self.coeff * turtle.x
+
 
 class YCor(Value):
     def __init__(self):
@@ -101,57 +112,63 @@ class YCor(Value):
     def value(self):
         return self.coeff * turtle.y
 
+
 class Heading(Value):
     def __init__(self):
-        Value.__init__(self) 
+        Value.__init__(self)
 
     def value(self):
         return self.coeff * turtle.angle
 
-#-------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------
 # liste des mots clés, avec le nom du token associé
 reserved = {
-    'forward' : 'FORWARD',
-    'backward' : 'BACKWARD',
-    'left' : 'LEFT',
-    'right' : 'RIGHT',
-    'color' : 'COLOR',
-    'pen' : 'PEN',
-    'up' : 'UP',
-    'down' : 'DOWN',
-    'repeat' : 'REPEAT',
-    'to' : 'TO',
-    'end' : 'END',
-    'xcor' : 'XCOR',
-    'ycor' : 'YCOR',
-    'heading' : 'HEADING'
+    'forward': 'FORWARD',
+    'backward': 'BACKWARD',
+    'left': 'LEFT',
+    'right': 'RIGHT',
+    'color': 'COLOR',
+    'pen': 'PEN',
+    'up': 'UP',
+    'down': 'DOWN',
+    'repeat': 'REPEAT',
+    'to': 'TO',
+    'end': 'END',
+    'xcor': 'XCOR',
+    'ycor': 'YCOR',
+    'heading': 'HEADING'
 }
 
-tokens = ['NUMBER', 'CROG', 'CROD', 'NAME', 'COL', 'PNAME'] + list(reserved.values())
+tokens = ['NUMBER', 'CROG', 'CROD', 'NAME',
+          'COL', 'PNAME'] + list(reserved.values())
 
-t_CROG    = r'\['
-t_CROD    = r'\]'
-t_COL     = r'\#[a-f0-9]{6}'
-t_PNAME   = r':[a-zA-Z_][a-zA-Z0-9_]*'
+t_CROG = r'\['
+t_CROD = r'\]'
+t_COL = r'\#[a-f0-9]{6}'
+t_PNAME = r':[a-zA-Z_][a-zA-Z0-9_]*'
 
 # Des nombres entiers; ici on a testé les débordements possibles
+
+
 def t_NUMBER(t):
     r'-?[0-9]+'
     try:
-        t.value = int(t.value) 
+        t.value = int(t.value)
     except ValueError:
         print("Valeur trop grande", t.value)
         t.value = 0
     return t
 
+
 def t_NAME(t):
     r'[a-zA-Z_][a-zA-Z0-9_]*'
     # la valeur associée est directement la chaîne de caractère lue
-    # rien à faire pour la valeur. Par contre le nom du token change 
+    # rien à faire pour la valeur. Par contre le nom du token change
     # si c'est un mot clé!
     # on cherche dans la liste des mots clés, et si on ne trouve pas
     # on renvoit 'NAME'
-    t.type = reserved.get(t.value, 'NAME') 
+    t.type = reserved.get(t.value, 'NAME')
     if t.type == 'XCOR':
         t.value = turtle.x
     elif t.type == 'YCOR':
@@ -161,27 +178,34 @@ def t_NAME(t):
 
     return t
 
+
 # caractères à ignorer: ici espaces, tabulations, saut de lignes
 t_ignore = " \t\n"
 
 # caractères inattendus: tout ce qui n'est pas défini avant
+
+
 def t_error(t):
     print("Caractère inattendu: ", t.value[0])
-    t.lexer.skip(1) # on saute ce caractère et on passe au suivant
+    t.lexer.skip(1)  # on saute ce caractère et on passe au suivant
+
 
 # instantiation de l'analyseur lexical
 lexer = lex.lex()
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # grammaire
 # On engendre maintenant des listes d'objets représentant les instructions
+
+
 def p_instructions(p):
     '''instructions : instruction
                     | instructions instruction'''
     if len(p) == 2:
-        p[0] =  p[1]
+        p[0] = p[1]
     else:
-        p[0] = p[1] + p[2] # concaténation des deux listes
+        p[0] = p[1] + p[2]  # concaténation des deux listes
+
 
 def p_instruction(p):
     '''instruction : FORWARD value
@@ -222,6 +246,7 @@ def p_instruction_to(p):
     procs[name] = Procedure(p[4], p[3])
     p[0] = []
 
+
 def p_instruction_to_call(p):
     'instruction : NAME values'
     name = p[1]
@@ -229,9 +254,9 @@ def p_instruction_to_call(p):
         print('Erreur: fonction inconnue :', name)
         exit(1)
 
-    proc =  procs[name]
+    proc = procs[name]
     if proc.parameters:
-        if  len(p[2]) < len(proc.parameters):
+        if len(p[2]) < len(proc.parameters):
             print("Erreur : pas assez d'arguments pour appeler", name)
             exit(1)
         elif len(p[2]) > len(proc.parameters):
@@ -247,43 +272,53 @@ def p_instruction_to_call(p):
     else:
         p[0] = proc.instructions
 
-        
+
 def p_value_n(p):
     'value : NUMBER'
     p[0] = Constant(p[1])
+
 
 def p_value_x(p):
     'value : XCOR'
     p[0] = XCor()
 
+
 def p_value_y(p):
     'value : YCOR'
     p[0] = YCor()
 
+
 def p_value_h(p):
     'value : HEADING'
     p[0] = Heading()
+
 
 def p_value_p(p):
     'value : PNAME'
     p[0] = Parameter(p[1])
 
 # values -> epsilon
+
+
 def p_values_empty(p):
     'values : '
     p[0] = []
+
 
 def p_values_list(p):
     'values : values value'
     p[0] = p[1] + [p[2]]
 
+
 def p_parameters_empty(p):
     'parameters : '
     p[0] = []
 
+
 def p_parameters_list(p):
     'parameters : parameters parameter'
     p[0] = p[1] + p[2]
+
 
 def p_parameter(p):
     'parameter : PNAME'
@@ -297,13 +332,15 @@ def p_error(p):
     else:
         print("Fin de chaîne inattendue")
 
+
 # instantiation de l'analyseur syntaxique
 parser = yacc.yacc()
 # ------------------------------------------------------------------------------
 
-# un nouveau dictionnaire donnant un objet de type Procedure pour chaque nom 
+# un nouveau dictionnaire donnant un objet de type Procedure pour chaque nom
 #  de procédure
 procs = {}
+
 
 class Procedure:
     def __init__(self, I, P):
@@ -311,6 +348,7 @@ class Procedure:
         self.parameters = P
 
 # ------------------------------------------------------------------------------
+
 
 # on teste le nombre d'arguments donnés sur la ligne de commande
 # la longueur de argv est 1 + ce nombre, la case argv[0] correspondant au nom du programme.
@@ -323,9 +361,15 @@ finput = open(argv[1], 'r')
 program = finput.read()
 finput.close()
 
+output_dir = "output/"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+output_name = os.path.basename(argv[1]).rsplit('.', 1)[0] + '.svg'
+
 # maintenant on écrit les résultats dans le fichier dont le nom consiste
 # en le nom du fichier logo où l'extension logo a été remplacée par svg
-foutput = open(argv[1].rsplit('.', 1)[0] + '.svg', 'w')
+foutput = open(output_dir + output_name, 'w')
 
 # une nouvelle tortue. On lui passe le fichier résultat pour qu'elle puisse
 # écrire dedans
@@ -333,8 +377,10 @@ turtle = Turtle(foutput)
 
 # entête du fichier svg
 foutput.write('<?xml version="1.0" standalone="no"?>\n')
-foutput.write('<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1/1/DTD/svg11.dtd">\n')
-foutput.write('<svg width="1000" height="600" version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
+foutput.write(
+    '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1/1/DTD/svg11.dtd">\n')
+foutput.write(
+    '<svg width="1000" height="600" version="1.1" xmlns="http://www.w3.org/2000/svg">\n')
 foutput.write('<rect width="100%" height="100%" fill="white"/>')
 foutput.write(f'<title>{argv[1]}</title>\n')
 
@@ -351,4 +397,3 @@ foutput.write('</svg>')
 
 # on n'oublie pas de fermer le fichier pour s'assurer que tout ce qui était potentiellement en attente d'écriture a été bien écrit.
 foutput.close()
-
